5668e59fc4828997912b60cff621126c4a7efd98
DiegoSanMo6011 <111369219+DiegoSanMo6011@users.noreply.github.com>
2022-09-21 22:40:43 -0500

    Add files via upload

--- TC1028_A01276011.py
+++ TC1028_A01276011.py
@@ -24,6 +24,7 @@ def fromula_gene(a,b,c):
     else:
         return "El valor de a no puede ser 0"
 def caida_altura(t,g):
+    
     h=1/2*g*t**2
     return h
 def caida_velfinh(h,g):
@@ -43,68 +44,92 @@ def respuesta (resp):
     print ("El valor que busca es:",resp)
 #error
 def error():
-    print("Ese no es un valor valido, corra el proograma de nuevo")
-#Menu
-ecuacion_n=int(input("¿Qué ecuación necesita?: 1. Pitagoras, 2. Regla de tres, 3.Formula general,4. Caida Libre"))
-if ecuacion_n==1:
-    valor=int(input("¿Qué valor quiere calcular?: 1. hipotenusa, 2. cateto"))
-    if valor==1:
-        cat_1=float(input("Ingresa un cateto"))
-        cat_2=float(input("Ingresa el otro cateto"))
-        respues=pitagorashipo(cat_1,cat_2)
-        respuestaf=respuesta(respues)
-    elif valor==2:
-        cat_1=float(input("Ingresa un cateto"))
-        Hipo=float(input("Ingresa la hipotenusa"))
-        respues=pitagorascateto(cat_1,Hipo)
-        respuestaf=respuesta(respues)
-    else:
-        error()
-elif ecuacion_n==2:
-    const_comp=float(input("Ingrese el valor de la constante de la cual conoce la variable"))
-    vari_comp=float(input("Ingrese el valor de la variable"))
-    const_inc=float(input("ngrese el valor de la constante de la cual NO conoce la variable"))
-    respues=regla_de_tres(vari_comp,const_inc,const_comp)
-    respuestaf=respuesta(respues)
-elif ecuacion_n==3:
-    cuadrado=float(input("Ingrese el valor que tiene una incognita elevada al cuadrado"))
-    expo=float(input("Ingrese el valor que tiene una incognita"))
-    sinexpo=float(input("Ingrese el valor sin incognita"))
-    respues=fromula_gene(cuadrado,expo,sinexpo)
-    respuestaf=respuesta(respues)
-elif ecuacion_n==4:
-    gravedad=9.8
-    valor=int(input("¿Qué valor quiere calcular?: 1. Altura, 2. Velocidad final, 3. Tiempo"))
-    if valor==1:
-        tiempo=int(input("Ingrese el tiempo"))
-        respues=caida_altura(tiempo,gravedad)
-        respuestaf=respuesta(respues)
-    elif valor==2:
-        datos=int(input("¿Qué dato quiere utilizar para encontar el valor?, 1. ALtura, 2.Tiempo"))
-        if datos==1:
-            altura=float(input("Ingrese la altura"))
-            respues=caida_velfinh(altura,gravedad)
+    print("Ese no es un valor valido, corra el programa de nuevo")
+#Iniciar/salir Programa
+ban=int(input("Introduce 1, para iniciar el programa"))
+while ban==1:
+    #Menu
+    ecuacion_n=int(input("¿Qué ecuación necesita?: 1. Pitagoras, 2. Regla de tres, 3.Formula general,4. Caida Libre,5. Salir"))
+    #pitagoras
+    if ecuacion_n==1:
+        valor=int(input("¿Qué valor quiere calcular?: 1. hipotenusa, 2. cateto"))
+        #hipotenusa
+        if valor==1:
+            cat_1=float(input("Ingresa un cateto"))
+            cat_2=float(input("Ingresa el otro cateto"))
+            respues=pitagorashipo(cat_1,cat_2)
             respuestaf=respuesta(respues)
-        elif datos==2:
+        #cateto
+        elif valor==2:
+            cat_1=float(input("Ingresa un cateto"))
+            Hipo=float(input("Ingresa la hipotenusa"))
+            if (Hipo>cat_1):
+                respues=pitagorascateto(Hipo,cat_1)
+                respuestaf=respuesta(respues)
+            else:
+                error()
+        else:
+            error()
+        ban=1
+    #regla de tres
+    elif ecuacion_n==2:
+        const_comp=float(input("Ingrese el valor de la constante de la cual conoce la variable"))
+        vari_comp=float(input("Ingrese el valor de la variable"))
+        const_inc=float(input("ngrese el valor de la constante de la cual NO conoce la variable"))
+        respues=regla_de_tres(vari_comp,const_inc,const_comp)
+        respuestaf=respuesta(respues)
+        ban=1
+    #Formula general
+    elif ecuacion_n==3:
+        cuadrado=float(input("Ingrese el valor que tiene una incognita elevada al cuadrado"))
+        expo=float(input("Ingrese el valor que tiene una incognita"))
+        sinexpo=float(input("Ingrese el valor sin incognita"))
+        respues=fromula_gene(cuadrado,expo,sinexpo)
+        respuestaf=respuesta(respues)
+        ban=1
+    #Caida libre
+    elif ecuacion_n==4:
+        gravedad=9.8
+        valor=int(input("¿Qué valor quiere calcular?: 1. Altura, 2. Velocidad final, 3. Tiempo"))
+        #altura
+        if valor==1:
             tiempo=int(input("Ingrese el tiempo"))
-            respues=caida_velfint(gravedad,tiempo)
+            respues=caida_altura(tiempo,gravedad)
             respuestaf=respuesta(respues)
+        #Velocidad final
+        elif valor==2:
+            datos=int(input("¿Qué dato quiere utilizar para encontar el valor?, 1. ALtura, 2.Tiempo"))
+            if datos==1:
+                altura=float(input("Ingrese la altura"))
+                respues=caida_velfinh(altura,gravedad)
+                respuestaf=respuesta(respues)
+            elif datos==2:
+                tiempo=int(input("Ingrese el tiempo"))
+                respues=caida_velfint(gravedad,tiempo)
+                respuestaf=respuesta(respues)
+            else:
+                error()
+        #tiempo
+        elif valor==3:
+            datos=int(input("¿Qué dato quiere utilizar para encontar el valor?, 1. ALtura, 2. Velocidad final"))
+            if datos==1:
+                altura=float(input("Ingrese la altura"))
+                respues=caida_tiempoh(altura,gravedad)
+                respuestaf=respuesta(respues)
+            elif datos==2:
+                velfin=float(input("Ingrese la velocidad final"))
+                respues=caida_tiempovf(velfin,gravedad)
+                respuestaf=respuesta(respues)
+            else:
+                error()
         else:
             error()
-    elif valor==3:
-        datos=int(input("¿Qué dato quiere utilizar para encontar el valor?, 1. ALtura, 2. Velocidad final"))
-        if datos==1:
-            altura=float(input("Ingrese la altura"))
-            respues=caida_tiempoh(altura,gravedad)
-            respuestaf=respuesta(respues)
-        elif datos==2:
-            velfin=float(input("Ingrese la velocidad final"))
-            respues=caida_tiempovf(velfin,gravedad)
-            respuestaf=respuesta(respues)
-        else:
-            error()
+        ban=1
+    elif ecuacion_n==5:
+        ban=0
     else:
         error()
 else:
-    error()
+    print("hasta pronto")
+
 
